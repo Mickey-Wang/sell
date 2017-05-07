1b0f987d8fe0bd8825d7f4d2df0663b2eccb443d
王琪(Mickey Wang) <myday19920506@me.com>
2017-05-07 23:56:39 +0800

    header部分布局样式开发

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,13 +14,18 @@
         </div>
         <!--这里加if判断是避免ajax数据响应之前渲染数据时报错-->
         <div class="support" v-if="seller.supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
     <div class="bulletin-wrapper">
-
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
 </template>
@@ -31,6 +36,9 @@
       seller: {
         type: Object
       }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
     }
   }
 </script>
@@ -40,19 +48,21 @@
 
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
-      font-size: 0//为了100%还原设计稿，使得头像与右侧内容间隔32px，所以需要去掉之间的空白字符，然后再添加margin-left
+      font-size: 0//为了100%还原设计稿，使得头像与右侧内容间隔32px，所以需要去掉之间的空白字符，然后再添加margin-left;在后边每个 最末级 文字容器中添加font-size覆盖此属性
       .avatar
         display: inline-block
+        img
+          border-radius: 2px
       .content
+        vertical-align: top // http://www.w3school.com.cn/cssref/pr_pos_vertical-align.asp
         display: inline-block
         margin-left: 16px
-        font-size: 14px//继承属性，覆盖
         .title
           margin: 2px 0 8px
-          font-size: 0
           .brand
             display: inline-block
             vertical-align: top
@@ -65,4 +75,77 @@
             font-size: 16px
             line-height: 18px
             font-weight: bold
+        .description
+          margin-bottom: 12px
+          line-height: 12px
+          font-size: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+            background-size: 12px 12px
+            background-repeat: no-repeat
+          .text
+            vertical-align: top
+            line-height: 12px
+            font-size: 10px
+
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0px 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        text-align: center
+        .count
+          font-size: 10px
+          vertical-align: middle
+        .icon-keyboard_arrow_right
+          vertical-align: middle
+          margin-left: 2px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 28px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      /*font-size: 0px 不能用这种去掉空白字符，会影响到三个点的显示，所以删掉dom中缩进的空白字符*/
+      .bulletin-title
+        display: inline-block
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+        vertical-align: top
+        margin-top: 8px // (28-12)/2 让title和text都顶端对齐，然后控制其中一个居中
+      .bulletin-text
+        margin-left: 4px
+        vertical-align: top
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+
 </style>
